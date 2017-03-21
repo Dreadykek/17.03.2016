a4fb0f55847caf24a862391d7b4d7d028aa0a9cf
Andrew Andersen <vlasov-andrey.oui@yandex.ru>
2017-03-21 13:33:24 +0500

    Problem 4 (Andrew's version)

---
+++ 4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+long long lastIn(const string &s, const string &p) {
+
+    long long con = p.length() - 1;
+
+    for (long long i = s.length() - 1; i >= 0; --i)
+        if (s[i] == p[con]) {
+            --con;
+            if (con == -1)
+                return i;
+        }
+        else {
+            con = p.length() - 1;
+        }
+}
+
+int main() {
+    string s, p;
+    getline(cin, s);
+    getline(cin, p);
+    cout << lastIn(s, p) << endl;
+}
